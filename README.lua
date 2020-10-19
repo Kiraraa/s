ab83d659477ab05428a2ee55e32c3ecbab91f09e
Kiraraa <73115630+Kiraraa@users.noreply.github.com>
2020-10-20 06:39:26 +0700

    README.lua

--- README.lua
+++ README.lua
@@ -1,8 +1,3 @@
---| Kirara - v3rmillion.net
---| I็็#9745 (701375651445211137) - discord.com
---| I have very small knowledge about Roblox Lua.
---| I make really simple GUIs.
+--\\ Kirara - v3rmillion.net
 
-if 2 + 2 - 1 == 3 then
-  print("Quick Math")
-end
+--// I็็#9745 (701375651445211137) - discord.com

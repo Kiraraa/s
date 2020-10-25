cf8d1cb303ed1db7ae1376070de9f45486527c99
Kiraraa <73115630+Kiraraa@users.noreply.github.com>
2020-10-25 13:01:55 +0700

    1.lua README

---
+++ 1readme.lua
@@ -0,0 +1,16 @@
+--// README for Elemental Battlegrounds GUI
+
+--[[ MAPS
+If the map that you teleported to is locked, use the Map Lock Bypass in miscellaneous.
+Drop Zone = an area where shards and diamonds drop.
+More players in the server = higher drop rate.
+At Minilovania, you can use "Sans Element" for free.
+]]
+
+--[[ FARM EXP
+You need the element and the 2nd spell for the exp farm.
+You can only use one at a time.
+Fire element is recommended.
+]]
+
+--// Last thing, let me know if something isn't working.
